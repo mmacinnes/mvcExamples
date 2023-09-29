95b5584f1da5d21b53218ad4a30d8f5fc226332a
mmacinnes <mmacinnes@conestogac.on.ca>
2023-09-29 18:35:14 -0400

    InitialCreation

---
+++ ContosoUniversity/Data/ApplicationDbContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Student> Student { get; set; } = default!;
+    public DbSet<Course> Course { get; set; } = default!;
+    public DbSet<Enrollment> Enrollments { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Student>()
+            .HasData( 
+                    new Student{StudentID=10001,FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
+                    new Student{StudentID=10002,FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                    new Student{StudentID=10003,FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
+                    new Student{StudentID=10004,FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                    new Student{StudentID=10005,FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                    new Student{StudentID=10006,FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
+                    new Student{StudentID=10007,FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
+                    new Student{StudentID=10008,FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
+            );
+                        modelBuilder.Entity<Course>().HasData( 
+                new Course{CourseID=1050,Title="Chemistry",Credits=3},
+                new Course{CourseID=4022,Title="Microeconomics",Credits=3},
+                new Course{CourseID=4041,Title="Macroeconomics",Credits=3},
+                new Course{CourseID=1045,Title="Calculus",Credits=4},
+                new Course{CourseID=3141,Title="Trigonometry",Credits=4},
+                new Course{CourseID=2021,Title="Composition",Credits=3},
+                new Course{CourseID=2042,Title="Literature",Credits=4}
+            );
+
+
+            modelBuilder.Entity<Enrollment>()
+                .HasData( 
+                    new Enrollment{EnrollmentID=1,StudentID=10001,CourseID=1050,Grade=Grade.A},
+                    new Enrollment{EnrollmentID=2,StudentID=10001,CourseID=4022,Grade=Grade.C},
+                    new Enrollment{EnrollmentID=3,StudentID=10001,CourseID=4041,Grade=Grade.B},
+                    new Enrollment{EnrollmentID=4,StudentID=10002,CourseID=1045,Grade=Grade.B},
+                    new Enrollment{EnrollmentID=5,StudentID=10002,CourseID=3141,Grade=Grade.F},
+                    new Enrollment{EnrollmentID=6,StudentID=10002,CourseID=2021,Grade=Grade.F},
+                    new Enrollment{EnrollmentID=7,StudentID=10003,CourseID=1050},
+                    new Enrollment{EnrollmentID=8,StudentID=10004,CourseID=1050},
+                    new Enrollment{EnrollmentID=9,StudentID=10004,CourseID=4022,Grade=Grade.F},
+                    new Enrollment{EnrollmentID=10,StudentID=10005,CourseID=4041,Grade=Grade.C},
+                    new Enrollment{EnrollmentID=11,StudentID=10006,CourseID=1045},
+                    new Enrollment{EnrollmentID=12,StudentID=10007,CourseID=3141,Grade=Grade.A}
+            );
+
+        }
+}
---
+++ ContosoUniversity/Data/Migrations/20230929160318_ApplicationTables.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ContosoUniversity.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplicationTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Course",
+                columns: table => new
+                {
+                    CourseID = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Credits = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.CourseID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    StudentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstMidName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EnrollmentDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.StudentID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enrollments",
+                columns: table => new
+                {
+                    EnrollmentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseID = table.Column<int>(type: "int", nullable: false),
+                    StudentID = table.Column<int>(type: "int", nullable: false),
+                    Grade = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enrollments", x => x.EnrollmentID);
+                    table.ForeignKey(
+                        name: "FK_Enrollments_Course_CourseID",
+                        column: x => x.CourseID,
+                        principalTable: "Course",
+                        principalColumn: "CourseID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Enrollments_Student_StudentID",
+                        column: x => x.StudentID,
+                        principalTable: "Student",
+                        principalColumn: "StudentID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Course",
+                columns: new[] { "CourseID", "Credits", "Title" },
+                values: new object[,]
+                {
+                    { 1045, 4, "Calculus" },
+                    { 1050, 3, "Chemistry" },
+                    { 2021, 3, "Composition" },
+                    { 2042, 4, "Literature" },
+                    { 3141, 4, "Trigonometry" },
+                    { 4022, 3, "Microeconomics" },
+                    { 4041, 3, "Macroeconomics" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Student",
+                columns: new[] { "StudentID", "EnrollmentDate", "FirstMidName", "LastName" },
+                values: new object[,]
+                {
+                    { 10001, new DateTime(2005, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Carson", "Alexander" },
+                    { 10002, new DateTime(2002, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Meredith", "Alonso" },
+                    { 10003, new DateTime(2003, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Arturo", "Anand" },
+                    { 10004, new DateTime(2002, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Gytis", "Barzdukas" },
+                    { 10005, new DateTime(2002, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Yan", "Li" },
+                    { 10006, new DateTime(2001, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Peggy", "Justice" },
+                    { 10007, new DateTime(2003, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Laura", "Norman" },
+                    { 10008, new DateTime(2005, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Nino", "Olivetto" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Enrollments",
+                columns: new[] { "EnrollmentID", "CourseID", "Grade", "StudentID" },
+                values: new object[,]
+                {
+                    { 1, 1050, 0, 10001 },
+                    { 2, 4022, 2, 10001 },
+                    { 3, 4041, 1, 10001 },
+                    { 4, 1045, 1, 10002 },
+                    { 5, 3141, 4, 10002 },
+                    { 6, 2021, 4, 10002 },
+                    { 7, 1050, null, 10003 },
+                    { 8, 1050, null, 10004 },
+                    { 9, 4022, 4, 10004 },
+                    { 10, 4041, 2, 10005 },
+                    { 11, 1045, null, 10006 },
+                    { 12, 3141, 0, 10007 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollments_CourseID",
+                table: "Enrollments",
+                column: "CourseID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollments_StudentID",
+                table: "Enrollments",
+                column: "StudentID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Enrollments");
+
+            migrationBuilder.DropTable(
+                name: "Course");
+
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
